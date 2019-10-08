99133f49365f51703c34b73b12d511eaed3f53e6
Kota Someya <33100724+Ksomeya1@users.noreply.github.com>
2019-10-08 14:34:49 -0700

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div className="footerContainer">
+        <span className="text-muted"> This is our footer content. </span>
+    </div>
+  </footer>
+);
+
+export default Footer;
